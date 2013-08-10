89d47d54753320552f132dce97dd646486464329
Jon San Miguel <sanmiguelje@yahoo.com>
2013-08-10 04:21:05 -0700

    Removed ruby patch from rvm prompt segment

--- segments/rvm_prompt.py
+++ segments/rvm_prompt.py
@@ -3,7 +3,7 @@ import subprocess
 
 def add_rvm_prompt_segment():
     try:
-        version = ' %s ' % " ".join(subprocess.check_output(['rvm-prompt', 'v', 'p', 'g'], stderr=subprocess.STDOUT).split())
+        version = ' %s ' % " ".join(subprocess.check_output(['rvm-prompt', 'v', 'g'], stderr=subprocess.STDOUT).split())
         powerline.append(version, Color.RVM_PROMPT_FG, Color.RVM_PROMPT_BG)
     except OSError:
         return
