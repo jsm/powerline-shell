e281ee3b299e2b27b206c3f321603d0a710eef27
Jon San Miguel <sanmiguelje@yahoo.com>
2013-10-30 14:58:34 -0700

    PWD instead of CWD

--- powerline-shell.py
+++ powerline-shell.py
@@ -212,7 +212,7 @@ def get_short_path(cwd):
 
 
 def add_cwd_segment():
-    cwd = powerline.cwd or os.getenv('PWD')
+    cwd = os.getenv('PWD') # or powerline.cwd
     names = get_short_path(cwd.decode('utf-8'))
 
     max_depth = powerline.args.cwd_max_depth
